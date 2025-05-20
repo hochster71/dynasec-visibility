bd495721608abef4b3d4a70ff059d1aecd12cbdd
hochster71 <michael.b.hoch@gmail.com>
2025-05-20 16:42:01 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDEezpF_02kkxk3R96Zh1ez3SuT4E2QUg8",
+  authDomain: "dynasec-cic.firebaseapp.com",
+  projectId: "dynasec-cic",
+  storageBucket: "dynasec-cic.appspot.com",
+  messagingSenderId: "517529086063",
+  appId: "1:517529086063:web:70c6ca5ec15655d480f9be",
+  measurementId: "G-2LNDQHW8T7"
+};
+firebase.initializeApp(firebaseConfig);
